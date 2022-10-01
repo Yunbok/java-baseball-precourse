fff00fbbe176b8a38a89d8cfab99f8dca53c473b
배윤복 <qodbsqhr@naver.com>
2022-10-02 02:36:52 +0900

    feat: 발생한 힌트(스트라이크, 볼, 낫싱)를 출력하는 기능
    - 볼과 스트라이크는 갯수와 같이 출력

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,18 +1,26 @@
 package baseball.controller;
 
+import baseball.domain.Computer;
 import baseball.domain.GamePlay;
 import baseball.domain.Player;
+import baseball.domain.Score;
 import baseball.view.ConsoleInput;
+import baseball.view.ConsoleOutput;
+
+import java.util.List;
 
 public class GameController {
 
     public static void start() {
 
+        Computer computer = new Computer();
         Player player = new Player();
         GamePlay gamePlay = new GamePlay();
 
         ConsoleInput.inputMessage();
-        gamePlay.addToPlayerNumbers(player.inputNumber());
+        List<Integer> playerNumbers = gamePlay.initPlayerNumbers(player.inputNumber());
+        Score score = computer.compareAnswers(playerNumbers);
+        ConsoleOutput.printResult(score.getScoreMessage());
 
     }
 }
---
+++ src/main/java/baseball/view/ConsoleOutput.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+public class ConsoleOutput {
+
+    private ConsoleOutput() {
+
+    }
+
+    public static void printResult(final String message) {
+        System.out.println(message);
+    }
+}
